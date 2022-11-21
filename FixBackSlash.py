c848caec1627fb1211d1c7c1a9cdfacd0181da92
trr5026 <53919167+trr5026@users.noreply.github.com>
2022-11-21 11:54:34 -0700

    Add files via upload

---
+++ FixBackSlash.py
@@ -0,0 +1,26 @@
+def ReplaceBackslash(Pathname):
+    ''' (str) -> str
+
+    Reads a file name and path with '\' slashes and returns the same name
+    with '/' slashes. Make sure to begin string input with 'r' so it is read as
+    a raw string which prevents python from converting certain backslash 
+    characters
+
+    >>> 'C:\Osers\Tim'
+    'C:/Osers/Tim'
+    '''
+
+    Pathname = 'r' + Pathname
+    
+    return Pathname.replace("\\","/")
+#    
+#    for char in Pathname:
+#        if Pathname[char] == "\\":
+#            newstring = '/'
+#        else:
+#            newstring = Pathname[char]
+    
+    
+            
+            
+        
